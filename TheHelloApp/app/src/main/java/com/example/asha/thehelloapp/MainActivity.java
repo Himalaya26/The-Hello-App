223f761501599652aa489a4cc04af03f6856520d
Himalaya S Verma <himalaya26feb@gmail,.com>
2018-01-23 10:10:31 +0530

    initial commit

---
+++ TheHelloApp/app/src/main/java/com/example/asha/thehelloapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.asha.thehelloapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clk(View view) {
+
+        EditText et = (EditText) findViewById(R.id.et);
+        Toast.makeText(getApplicationContext(), "Hello " + et.getText().toString() + "!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
